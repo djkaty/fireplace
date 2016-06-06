0941e8a1d520dc51834b583956b8e4b8f79f92e5
Katy Coe <djkaty@start.no>
2016-06-06 20:53:17 +0200

    Fix crash in pre-mulligan phase

--- kettle/kettle.py
+++ kettle/kettle.py
@@ -13,7 +13,7 @@ from hearthstone.enums import (
 )
 from fireplace import actions, cards
 from fireplace.exceptions import GameOver
-from fireplace.game import BaseGame as Game
+from fireplace.game import Game
 from fireplace.player import Player
 from fireplace.utils import CardList
 from fireplace.controller import GameController
@@ -179,6 +179,10 @@ class KettleManager:
 
 	def refresh_options(self):
 		if self.options_sent:
+			return
+
+		# Pre-mulligan phase?
+		if not self.game.current_player:
 			return
 
 		DEBUG("Refreshing options...")
